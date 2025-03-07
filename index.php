e1932b59fa4b080695f7fac972f7d2b86c0301e6
djanny-kas <djannykasika1804@gmail.com>
2025-03-07 12:39:55 +0200

    la part de kmg

--- index.php
+++ index.php
@@ -10,8 +10,14 @@ $questions = json_decode(file_get_contents('questions.json'), true);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Quiz interactif</title>
     <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
+    <header>
+        <?php 
+            include_once('header.php')
+        ?>
+    </header>
     <div class="quiz-container">
         <h1>Quiz Interactif</h1>
         <form id="quizForm">
